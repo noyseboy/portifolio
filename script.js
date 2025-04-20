ccb68de7b19a2ebaa2bfb5ccf88a9f16b7fef757
thyago silva <131825997+noyseboy@users.noreply.github.com>
2025-04-20 15:01:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("#mensagem-botao").addEventListener("click", function() {
+    const mensagem = document.querySelector("#mensagem");
+    mensagem.textContent = "Seja bem-vindo!Fico feliz com sua visita.!😊";
+});
